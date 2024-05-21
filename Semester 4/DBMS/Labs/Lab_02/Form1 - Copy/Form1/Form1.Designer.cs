7da9cb2b55ebe87390c105e846eff5ee5dd2d874
923-Groza-Iulia <115407815+923-Groza-Iulia@users.noreply.github.com>
2024-05-21 14:24:22 +0300

    Add files via upload

---
+++ Semester 4/DBMS/Labs/Lab_02/Form1 - Copy/Form1/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace Form1
+{
+    partial class Form1
+    {
+       
+        private System.ComponentModel.IContainer components = null;
+
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.animalsDataGridView = new System.Windows.Forms.DataGridView();
+            this.animalCareDataGridView = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.animalsDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.animalCareDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // albumsDataGridView
+            // 
+            this.animalsDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.animalsDataGridView.Location = new System.Drawing.Point(833, 12);
+            this.animalsDataGridView.Name = "animalsDataGridView";
+            this.animalsDataGridView.RowHeadersWidth = 51;
+            this.animalsDataGridView.RowTemplate.Height = 24;
+            this.animalsDataGridView.Size = new System.Drawing.Size(438, 286);
+            this.animalsDataGridView.TabIndex = 0;
+            // 
+            // songsDataGridView
+            // 
+            this.animalCareDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.animalCareDataGridView.Location = new System.Drawing.Point(302, 12);
+            this.animalCareDataGridView.Name = "animalCareDataGridView";
+            this.animalCareDataGridView.RowHeadersWidth = 51;
+            this.animalCareDataGridView.RowTemplate.Height = 24;
+            this.animalCareDataGridView.Size = new System.Drawing.Size(478, 286);
+            this.animalCareDataGridView.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.AccessibleDescription = "updateButton";
+            this.button1.AccessibleName = "updateButton";
+            this.button1.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.button1.Location = new System.Drawing.Point(759, 368);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 50);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "update";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.updateEvent);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(527, 329);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "AnimalCare";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1035, 329);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Animals";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1717, 716);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.animalCareDataGridView);
+            this.Controls.Add(this.animalsDataGridView);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.animalsDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.animalCareDataGridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView animalsDataGridView;
+        private System.Windows.Forms.DataGridView animalCareDataGridView;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Semester 4/DBMS/Labs/Lab_02/Form1 - Copy/Form1/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+
+namespace Form1
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection conn;
+        SqlDataAdapter daAnimals;
+        SqlDataAdapter daAnimalCare;
+        DataSet dset;
+        BindingSource bsAnimals;
+        BindingSource bsAnimalCare;
+        SqlCommandBuilder cmdBuilder;
+        string queryAnimals;
+        string queryAnimalCare;
+        String parentTable;
+        String childTable;
+        String foreignKey;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            FillData();
+        }
+        /*private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show(ConfigurationManager.AppSettings["greeting"]);
+        }*/
+        void FillData()
+        {
+            parentTable = ConfigurationManager.AppSettings["Parent"];
+            childTable = ConfigurationManager.AppSettings["Child"];
+            foreignKey = ConfigurationManager.AppSettings["foreignKey"];
+
+            conn = new SqlConnection(getConnectionString());
+            queryAnimals = "SELECT * FROM " + parentTable;
+            queryAnimalCare = "SELECT * FROM " + childTable;
+
+            daAnimals=new SqlDataAdapter(queryAnimals, conn);
+            daAnimalCare = new SqlDataAdapter(queryAnimalCare, conn);
+            dset=new DataSet();
+            daAnimals.Fill(dset,parentTable);
+            daAnimalCare.Fill(dset, childTable);
+            cmdBuilder = new SqlCommandBuilder(daAnimalCare);
+
+            dset.Relations.Add("AnimalsAnimalCare",
+                dset.Tables[parentTable].Columns[foreignKey],
+                dset.Tables[childTable].Columns[foreignKey]);
+
+            /*  this.AnimalsGridView.DataSource = dset.Tables["Animals"];
+              this.AnimalCareGridView.DataSource = this.AnimalsGridView.DataSource;
+              this.AnimalCareGridView.DataMember = "AnimalsAnimalCare";
+  */
+
+            bsAnimals = new BindingSource();
+            bsAnimals.DataSource = dset.Tables[parentTable];
+            bsAnimalCare = new BindingSource(bsAnimals, "AnimalsAnimalCare");
+            this.animalsDataGridView.DataSource = bsAnimals;
+            this.animalCareDataGridView.DataSource = bsAnimalCare;
+
+            cmdBuilder.GetUpdateCommand();
+
+        }
+
+        private void updateEvent(object sender, EventArgs e)
+        {
+            try
+            {
+                daAnimalCare.Update(dset, childTable);
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message, "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Console.WriteLine(exception.ToString());
+            }
+        }
+        /*private void button1_Click(object sender, EventArgs e)
+        {
+            daAnimalCare.Update(dset, "AnimalCare");
+        }*/
+
+        string getConnectionString()
+        {
+            return "Data Source=LAPTOP-OQ89RHJ2\\SQLEXPRESS;" + "Initial Catalog=Circ;Integrated Security=true";
+        }
+
+    }
+}
